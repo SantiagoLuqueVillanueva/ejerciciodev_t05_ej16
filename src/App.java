31358893d1bc89766ae86737747f77df1bc5307e
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-30 22:38:09 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num = Integer.parseInt(System.console().readLine("Introduzca un número entero y le diré si es primo: "));
+        boolean esprimo = true;
+
+        for (int i = 2; i < num; i++) {
+            if ((num % i) == 0){
+                esprimo = false;
+            }
+        }
+        if (esprimo == true) {
+            System.out.println("El número introducido es primo.");
+        } else {
+            System.out.println("El número introducido no es primo.");
+        }
     }
 }
